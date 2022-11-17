59383849976f910dd5b6272d0b975b718546b67b
jdbacero <jdbacero@gmail.com>
2022-11-17 18:35:49 +0800

    Router redirects and aliases

--- src/router/index.js
+++ src/router/index.js
@@ -16,15 +16,16 @@ const routes = [
     })
   },
   {
-    path: "/about",
+    path: "/about-us",
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: About,
+    alias: '/about',
   },
   {
-    path: '/event/:id',
+    path: '/events/:id',
     name: 'EventLayout',
     props: true,
     component: EventLayout,
@@ -46,6 +47,32 @@ const routes = [
       },
     ]
   },
+  {
+    path: '/event/:afterEvent(.*)',
+    redirect: to => {
+      return { path: `/events/${to.params.afterEvent}` }
+    }
+  },
+  // {
+  //   path: '/event/:id',
+  //   redirect: () => {
+  //     return { name: 'EventDetails' }
+  //   },
+  //   children: [
+  //     {
+  //       path: 'register', redirect() { return { name: 'EventRegister' } }
+  //     },
+  //     {
+  //       path: 'edit', redirect() { return { name: 'EventEdit' } }
+  //     },
+  //   ]
+  // },
+  // {
+  //   path: '/event/:id',
+  //   redirect: to => {
+  //     return { name: 'EventDetails', params: { id: to.params.id } }
+  //   }
+  // },
 ];
 
 const router = createRouter({
